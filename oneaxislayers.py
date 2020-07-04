e373d0c19770721b9712fa0ed0c89dd163896a76
Sebastian Carewe <sebastian.carewe@gmail.com>
2020-07-04 20:16:48 +0200

    cleanup

--- oneaxislayers.py
+++ oneaxislayers.py
@@ -4,15 +4,14 @@ from drawBot import *
 # EDIT THESE VALUES #####################
 
 string="string"
-fontUwant="Your Variable Font"
-textsize=1000
+fontUwant="Your variable font"
+textsize=300
 
 fps=60
-speed=60
-# speed/fps=duration of one loop in seconds
+speed=60 # speed/fps=duration of one loop in seconds
 
 framewidth=1080
-frameheight=1920
+frameheight=1080
 
 layers=5
 
@@ -37,24 +36,26 @@ def draw(loopframe, astart):
     
     # set main text properties
     blendMode("overlay")
-    cmykFill(.1, 0, .2, 0)
+    cmykFill(.2, .1, 0, 0)
     font(fontUwant)
     fontSize(textsize)
     
+    # for the specified number of layers, calculate the respective values
     for l in range(layers):
         a = QuinticEaseInOut(start=astart, end=axis1max-l*((axis1max-axis1min)/(layers-1)), duration=speed)
         args1 = {axis1 : a.ease(loopframe)}
         fontVariations(**args1)
         text(string, (textPosX, textPosY), align="center")
 
-
-
+# create a static repetitition of draw()
 def pause(length, currstart):
     for z in range(length):
         draw(a1, currstart)
 
-    
+# for the specified number of loops, cycle through the number of frames defined in speed and insert them into draw()
 for k in range(loops):
+    
+    # second argument defines the start of the loop haöf, I'm sure there's a far more efficient way to write this
     for a1 in range(speed):
         draw(a1, axis1min)
     for a1 in reversed(range(speed)):
@@ -65,6 +66,5 @@ for k in range(loops):
     for a1 in reversed(range(speed)):
         draw(a1, axis1min)
     pause(10, axis1min)
-
-        
+    
 saveImage("~/Desktop/" + string + " " + str(layers) + " layers.mp4")
